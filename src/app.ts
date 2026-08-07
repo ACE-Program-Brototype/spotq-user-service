6d35a7849abc0c24c0f7b3d55226b74ea9c6a0fd
sumeshofficial <sumeshsujesh1233@gmail.com>
2026-08-07 20:17:22 +0530

    chore: remove unused dependencies

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,12 @@ import {
 	notFoundMiddleware,
 } from "@interfaces/http/middlewares/index.js";
 import { router } from "@interfaces/http/routes/index.routes.js";
-import compression from "compression";
-import cors from "cors";
 import express from "express";
-import helmet from "helmet";
 
 const app = express();
 
 app.use(loggerMiddleware);
 app.use(metricsMiddleware);
-app.use(helmet());
-app.use(cors());
-app.use(compression());
 app.use(express.json());
 
 app.use("/", router);
