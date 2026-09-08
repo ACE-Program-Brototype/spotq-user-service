f976eb4fbc100208750bb647c2919a5f55934f4d
Ajex Joshy <ajexjoshy@Ajexs-MacBook-Air.local>
2026-09-08 18:31:41 +0530

    style(di): add top-level module documentation and remove inline comments (SCRUM-55)

--- src/config/di/user/user.module.ts
+++ src/config/di/user/user.module.ts
@@ -1,3 +1,7 @@
+/**
+ * Inversify container module for user domain dependencies.
+ * Registers repositories, use cases, controllers, and routers.
+ */
 import { CustomerForgotPasswordUseCase } from "@application/use-cases/customer.forgot-password.ts";
 import { CustomerResetPasswordUseCase } from "@application/use-cases/customer.reset.password.ts";
 import { CustomerVerifyForgotPasswordUseCase } from "@application/use-cases/customer.verify.forgot-password.ts";
@@ -36,7 +40,6 @@ import { ContainerModule } from "inversify";
 import { USER_TYPES } from "./user.types.ts";
 
 export const userModule = new ContainerModule(({ bind }) => {
-	// Repositories
 	bind<IUserRepository>(USER_TYPES.UserRepository).to(PrismaUserRepository);
 	bind<IRefreshTokenRepository>(USER_TYPES.RefreshTokenRepository).to(
 		PrismaRefreshTokenRepository,
@@ -45,7 +48,6 @@ export const userModule = new ContainerModule(({ bind }) => {
 		PrismaDeviceRepository,
 	);
 
-	// Application Use Cases
 	bind<IRegisterUserUseCase>(USER_TYPES.RegisterUserUseCase).to(
 		RegisterUserUseCase,
 	);
@@ -65,7 +67,6 @@ export const userModule = new ContainerModule(({ bind }) => {
 		GetCustomerProfileUseCase,
 	);
 
-	// HTTP Controllers & Routers
 	bind<UserAuthController>(USER_TYPES.UserAuthController).to(
 		UserAuthController,
 	);
